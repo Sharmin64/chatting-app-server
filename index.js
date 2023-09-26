7c849f007cf2b8949c205adb6a8e5f31a096f424
Sharmin64 <lizaeatsbanana@gmail.com>
2023-09-26 14:28:48 +0600

    axios imported in openai.js file

---
+++ index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import OpenAI from "openai";
+const port = process.env.PORT || 5000;
+
+//configuration
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}));
+app.use(morgan("common"));
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+app.get("/", (req, res) => {
+  res.send("Chat app started");
+});
+
+//Open AI Configuration
+
+export const openai = new OpenAI({
+  apiKey: "OPEN_API_KEY", // defaults to process.env["OPENAI_API_KEY"]
+});
+//SERVer SetUp
+
+app.listen(port, () => {
+  console.log(`Chatting App listening at http://localhost:${port}`);
+});
