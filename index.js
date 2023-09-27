24ce76f88720ded62efd83bafd7fb59912c9fe7f
Sharmin64 <lizaeatsbanana@gmail.com>
2023-09-27 23:37:55 +0600

    openai file in index file path

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
 import OpenAI from "openai";
+import openAiRoutes from "./routes/openai.js";
+
 const port = process.env.PORT || 5000;
 
 //configuration
@@ -26,6 +28,9 @@ app.get("/", (req, res) => {
 export const openai = new OpenAI({
   apiKey: "OPEN_API_KEY", // defaults to process.env["OPENAI_API_KEY"]
 });
+
+//Routes;
+app.use("/openai", openAiRoutes);
 //SERVer SetUp
 
 app.listen(port, () => {
--- routes/openai.js
+++ routes/openai.js
@@ -2,3 +2,18 @@ import express from "express";
 import axios from "axios";
 import dotenv from "dotenv";
 import {openai} from "../index.js";
+
+dotenv.config();
+const router = express.Router();
+
+router.post("/test", async (req, res) => {
+  try {
+    const {text, activeChatId} = req.body;
+    console.log("text", text);
+    res.status(200).json({text});
+  } catch (error) {
+    console.error("error", error.message);
+  }
+});
+
+export default router;
